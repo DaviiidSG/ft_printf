e42b1df3a579f4a1916f52569fa83f250268be31
David <110501225+DaviiidSG@users.noreply.github.com>
2026-02-17 15:03:27 +0100

    feat: add header file

---
+++ src/ft_printf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dserrano <dserrano@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/08 22:33:09 by dserrano          #+#    #+#             */
+/*   Updated: 2026/02/17 05:18:43 by dserrano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft.h"
+# include <stdarg.h>
+
+int	ft_printf(char const *str, ...);
+int	ft_search_specifier(const char *str, va_list args);
+int	ft_parser(const char *str, int *i, va_list args);
+int	ft_print_char(va_list args);
+int	ft_print_str(va_list args);
+int	ft_print_ptr(va_list args);
+int	ft_putnbr_base(unsigned long long num, int base);
+
+#endif
